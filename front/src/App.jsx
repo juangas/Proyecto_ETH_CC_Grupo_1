398e3a05c725a08ecd0cc25e82226cfa9d1deba9
Antonio Fraga <am.fraga.led@gmail.com>
2023-11-27 17:11:46 +0100

    Add refs to draft pages: About,privacy, terms

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,11 +1,15 @@
-import './App.css';
+import './styles/App.css';
 
 function App() {
 	return (
-		<>
-			<h1>Proyecto ETH CC Grupo 1</h1>
-		</>
+		<div>
+			<div className='center'>
+				<h2>Ethereum Team Project - Team1</h2>
+			</div>
+			<div className='center'>
+				<h5>- under construction -</h5>
+			</div>
+		</div>
 	);
 }
-
 export default App;
