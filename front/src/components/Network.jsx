b8e9b5c04481c940e07b54265a2bc24da93823c0
Antonio Fraga <am.fraga.led@gmail.com>
2023-11-30 13:44:40 +0100

    Refactoring-Use react components instead of html

---
+++ front/src/components/Network.jsx
@@ -0,0 +1,12 @@
+//styles
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Network() {
+	return (
+		<div className="bg-white text-black text-center py-4 ">
+			<h2>Create and manage Private Ethereum Networks</h2>
+            <h5>--Under construction--</h5>
+		</div>
+	);
+}
+export default Network;
