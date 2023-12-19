ef2064124b79afd58c28baf956cd5ef9aa14c818
Pau MB <Pau MB>
2023-12-19 18:22:50 +0100

    remove in construction

--- front/src/views/NetworkManagement.jsx
+++ front/src/views/NetworkManagement.jsx
@@ -2,31 +2,28 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function NetworkManagement() {
-
 	const [nodeList, setNodeList] = useState('');
 
 	useEffect(() => {
 		// Invocar la ruta de Node.js al cargar el componente
 		fetch('http://localhost:3333/nodeList/')
-		  .then(response => response.text())
-		  .then(data => setNodeList(data))
-		  .catch(error => console.error('Error al obtener datos:', error));
-	  }, []);
+			.then(response => response.text())
+			.then(data => setNodeList(data))
+			.catch(error => console.error('Error al obtener datos:', error));
+	}, []);
 
 	return (
 		<div className='vh-100 overflow-auto bg-white text-black text-center py-4'>
 			<h4>Private Network Node List</h4>
 			<div>
-				<div style={{
-                fontSize: '14px', // Ajusta el tamaño de la fuente según sea necesario
-                whiteSpace: 'nowrap', // Evita saltos de línea
-                overflowX: 'auto', // Agrega una barra de desplazamiento horizontal si es necesario
-				}} 
-				dangerouslySetInnerHTML={{ __html: nodeList }} />
-			</div>
-			<div className='bg-white text-black text-center py-4 '>
-				<h4>Add / Remove nodes</h4>
-				<h5>--Under construction--</h5>
+				<div
+					style={{
+						fontSize: '14px', // Ajusta el tamaño de la fuente según sea necesario
+						whiteSpace: 'nowrap', // Evita saltos de línea
+						overflowX: 'auto', // Agrega una barra de desplazamiento horizontal si es necesario
+					}}
+					dangerouslySetInnerHTML={{ __html: nodeList }}
+				/>
 			</div>
 		</div>
 	);
